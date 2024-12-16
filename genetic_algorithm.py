e7afbdcbda986c2ad831ca48f15cd5748f90d67b
shawnyang84 <shawnyang@gamesoultech.com>
2024-12-16 16:55:04 +0800

    init;

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,12 +2,24 @@ import numpy as np
 import pandas as pd
 import copy
 
+def ulcer_index(data: np.array) -> float:
+    """
+    Calculate the Ulcer Index of a time series.
+    :param data: NumPy array of price data.
+    :return: Ulcer Index.
+    """
+    max_price = np.maximum.accumulate(data)
+    drawdown = (data - max_price) / max_price
+    squared_drawdown = np.square(drawdown)
+    mean_squared_drawdown = np.mean(squared_drawdown)
+    ulcer_index = np.sqrt(mean_squared_drawdown)
+    return ulcer_index
 class GeneticAlgoMiner:
     def __init__(self,
         num_of_candles: int,
         pattern_size: int,
-        population_size: int = 10,
-        num_of_generation: int = 20,
+        population_size: int = 200,
+        num_of_generation: int = 8,
         elitism_copys: int = 1,
         mutation_rate: float = 0.05,
         fresh_pattern_chance: float = 0.02,
@@ -53,28 +65,48 @@ class GeneticAlgoMiner:
             self._patterns.append(pattern)
         
     def train(self, data: pd.DataFrame):
+        # Calculate Ulcer Index
+        self._ulcer_index = ulcer_index(data['close'].to_numpy())
+
         # 1. Initialize patterns
         self._generate_random_pattern_rules()
 
         for i in range(self._num_of_generation):
-            print("Generation", i)
-            #self._train_generation(data)
+            # Reset values
+            self._next_generation_patterns = []
+            self._returns = [[] for _ in range(self._population_size)]
+
+            # Initailize fitness values
+            self._total_returns = [0] * self._population_size
+            self._profit_factors = [0] * self._population_size
+            self._martin_ratios = [0] * self._population_size
+
             # 2. Evaluate patterns with fitness functions
             self._evaluation(data)
 
-                # 3. Elitism
+            # 3. Elitism
             best_pattern = self._elite_selection()
             self._next_generation_patterns.append(best_pattern)
 
             # 4. Parent Selection and Reproduction
             self._parent_selection_and_reproduction()
-
+            
             # 5. Mutation
             self._mutation()
 
             # 6. Repeat from step 2
             self._patterns = copy.deepcopy(self._next_generation_patterns)
-
+            
+            # 7. Generation result and reset
+            print("Generation", i, "Population Best Fitness", self._martin_ratios[0], " Population Average Fitness", np.mean(self._martin_ratios))
+        print ("Final Pattern", self._patterns[0])
+        for i in range(5):
+            print("Final Pattern ", i)
+            print(self._patterns[i])
+            print("Total Returns ", self._total_returns[i], "Profit Factor ", self._profit_factors[i])
+            print("Average Return ", np.mean(self._returns[i]), "Time In Market ", len(self._returns[i]) / len(data))
+            print("Martin Ratio ", self._martin_ratios[i], "Ulcer Index ", self._ulcer_index)
+            
 
     def _calculate_fitness_functions(self):
         for i in range(self._population_size):
@@ -84,10 +116,10 @@ class GeneticAlgoMiner:
             positive_returns = abs(np.sum(returns_array > 0))
             negative_returns = abs(np.sum(returns_array < 0))
             self._profit_factors[i] = positive_returns / negative_returns if negative_returns != 0 else 0
-            # self._martin_ratios[i] = self._total_returns[i] / np.std(self._returns[i])
-        combined = list(zip(self._patterns, self._total_returns, self._profit_factors))
-        sorted_combined = sorted(combined, key=lambda x: x[1], reverse=True)
-        self._patterns, self._total_returns, self._profit_factors = zip(*sorted_combined)
+            self._martin_ratios[i] = self._total_returns[i] / self._ulcer_index
+        combined = list(zip(self._patterns, self._total_returns, self._profit_factors, self._martin_ratios))
+        sorted_combined = sorted(combined, key=lambda x: x[3], reverse=True)
+        self._patterns, self._total_returns, self._profit_factors, self._martin_ratios = zip(*sorted_combined)
         
     def _get_random_pattern(self, rules: list, pattern_size: int):
         rule_indices  = np.random.choice(len(rules), pattern_size, replace=False)
@@ -100,7 +132,7 @@ class GeneticAlgoMiner:
         :param data: NumPy array of K bars (each entry contains 'open', 'high', 'low', 'close').
         """
         for index, pattern in enumerate(self._patterns):
-            print("calculating pattern", index)
+            # print("calculating pattern", index)
             self._calculate_returns_with_single_pattern(pattern, index, data)
         self._calculate_fitness_functions()
 
@@ -111,26 +143,64 @@ class GeneticAlgoMiner:
         """
         max_index = 0
         for i in range(self._population_size):
-            if self._total_returns[i] <= 0 or self._profit_factors[i] <= 0:
+            if self._total_returns[i] <= 0 or self._profit_factors[i] <= 0 or self._martin_ratios[i] <= 0:
                 continue
             else:
                 max_index = i
                 break
-        print(self._patterns[max_index])
+
         return self._patterns[max_index]
             
     def _parent_selection_and_reproduction(self):
-        pass
+        while len(self._next_generation_patterns) < self._population_size:
+            # Select parents
+            parents = self._choose_parents()
+            # print("Original Parents", parents)
+
+            # Reproduce
+            rule1 = str(parents[0][0])
+            rule2 = str(parents[1][0])
+            
+            parents[0][0] = rule2
+            parents[1][0] = rule1
+            # print("New Parents", parents)
+            
+            if (len(self._next_generation_patterns) < self._population_size):
+                self._next_generation_patterns.append(parents[0])
+            if (len(self._next_generation_patterns) < self._population_size):
+                self._next_generation_patterns.append(parents[1])
+            # print("End of Reproduction")
+            
+    def _choose_parents(self):
+        parents = []
+        num_of_parents = 2
+
+        while len(parents) < num_of_parents:
+            seed = np.random.uniform(0, sum(self._martin_ratios))
+            parent_index = self._get_parent_index(seed)
+            if (self._patterns[parent_index] in parents):
+                # print("Duplicate Parent: ", "seed", seed, "parent_index", parent_index)
+                continue
+            
+            parents.append(self._patterns[parent_index])
+        return parents
+            
+    def _get_parent_index(self, seed: float):
+        accumulated = 0
+        for i in range(self._population_size):
+            if (seed <= self._martin_ratios[i] + accumulated):
+                return i
+            accumulated += self._martin_ratios[i]
+
+        return 0
 
     def _mutation(self):
         # Rule Adjustment
         num_of_rule_adjustments = int((self._mutation_rate - self._fresh_pattern_chance) * self._population_size)
         adjustments_indices = np.random.choice(self._population_size, num_of_rule_adjustments, replace=False)
-        print(adjustments_indices)
         for index in adjustments_indices:
-            print(index)
             pattern = self._next_generation_patterns[index]
-            print("Adjusting pattern", pattern)
+            # print("Adjusting pattern", pattern)
             # Choose symbol or index to adjust
             symbol_or_index = np.random.choice([0, 1])
             if symbol_or_index == 0:
@@ -148,11 +218,30 @@ class GeneticAlgoMiner:
                 pattern[symbol_index] = new_symbol + pattern[symbol_index][1:]
             else:
                 # Adjust index
-                index = np.random.choice(len(pattern))
-                index = np.random.choice(self._num_of_candles)
-                pattern[index] = pattern[index][0] + f'[{index}]' + pattern[index][3:]
-                
-            print("After Adjusted pattern", pattern)
+                should_continue = True
+                while should_continue:
+                    candle_index = np.random.choice(self._num_of_candles)
+                    rule_index = np.random.choice(self._pattern_size)
+                    
+                    # Split the string at the character you want to replace
+                    current_pattern = pattern[rule_index]
+                    
+                    # Find the position of the bracketed index you want to change
+                    split_position = current_pattern.find(']') - 1
+                    if pattern[rule_index][2] == str(candle_index):
+                        # print('Same index')
+                        continue
+                    else:
+                        # Replace the part within the brackets with the new index
+                        new_pattern = (
+                            current_pattern[:split_position] +
+                            str(candle_index) +
+                            current_pattern[split_position + 1:]
+                        )
+                        pattern[rule_index] = new_pattern  # Update the pattern with the new string
+                    should_continue = False
+
+            # print("After Adjusted pattern", pattern)
         
         # Fresh Pattern Generation
         num_of_fresh_patterns = int(self._fresh_pattern_chance * self._population_size)
@@ -183,7 +272,7 @@ class GeneticAlgoMiner:
         for index, rule in enumerate(pattern):
             # Parse the rule
             symbol1, index1, operator, symbol2, index2 = self._parse_rule(rule)
-            # print(start_index, index1, index2)
+
             # Get values from data
             value1 = data.iloc[start_index - index1][self._symbol_to_symbol(symbol1)]
             value2 = data.iloc[start_index - index2][self._symbol_to_symbol(symbol2)]
